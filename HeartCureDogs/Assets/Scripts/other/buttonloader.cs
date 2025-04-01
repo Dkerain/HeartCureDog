f2da65d368c70f2fce67ceb67dbcd956c9ca6ffa
Dkerain <165006578+Dkerain@users.noreply.github.com>
2025-04-01 17:20:58 +0800

    对话系统嵌进去第一章第一幕了
    对话系统还没做选项功能，到时候选项功能和属性系统一起做了

--- HeartCureDogs/Assets/Scripts/other/buttonloader.cs
+++ HeartCureDogs/Assets/Scripts/other/buttonloader.cs
@@ -7,19 +7,32 @@ using UnityEngine.UI; // 引用UI命名空间以访问Button类
 public class ButtonLoader : MonoBehaviour
 {
     public string sceneName; // 要跳转的场景名称
-
+    public Button button;//要有跳转功能的按钮
     void Start()
     {
-        // 获取按钮组件
-        Button button = GetComponent<Button>();
+        if (button == null)
+        {
+            button = GetComponent<Button>();
+        }
+        if (button != null)
+        {
+            button.onClick.AddListener(LoadScene);
+        }
+        //// 获取按钮组件
+        //Button button = GetComponent<Button>();
 
-        // 为按钮添加点击事件监听器
-        button.onClick.AddListener(LoadScene);
+        //// 为按钮添加点击事件监听器
+        //button.onClick.AddListener(LoadScene);
     }
-
     public void LoadScene()
     {
-        // 点击时加载指定场景
-        SceneManager.LoadScene(sceneName);
+        if (!string.IsNullOrEmpty(sceneName))
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+        else
+        {
+            Debug.Log("加载场景失败，场景为空");
+        }
     }
 }
