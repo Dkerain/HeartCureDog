b172109631ad7524b0491db2d574be8b37f62372
Dkerain <165006578+Dkerain@users.noreply.github.com>
2025-04-23 21:35:57 +0800

    提交对话选项系统
    不管了先交上去了，我这边还是点不了开始好奇怪，交代码脚本应该没问题

---
+++ HeartCureDogs/Assets/Scripts/LxtDialogueManager/Character.cs
@@ -0,0 +1,10 @@
+public class Character
+{
+    public string name;
+    public string emotionValue;
+    public int healthValue;
+    public int energyValue;//精力值
+    public int believeValue;
+    public int coinValue;
+    public int brwanValue;//体力值
+}
