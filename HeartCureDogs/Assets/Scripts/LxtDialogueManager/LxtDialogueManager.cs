834a72db202c7b19b36e7bd8d601363d1aea6234
woowanlianyee <l541735397@163.com>
2025-05-15 00:38:00 +0800

    Revert "在剧情模式下禁用小狗控制脚本"
    This reverts commit 7693970027a836654146877d062c2e98c7ce2e18.

--- HeartCureDogs/Assets/Scripts/LxtDialogueManager/LxtDialogueManager.cs
+++ HeartCureDogs/Assets/Scripts/LxtDialogueManager/LxtDialogueManager.cs
@@ -181,7 +181,7 @@ public class LxtDialogueManager : MonoBehaviour
             {
                 if (npc.name == _target)
                 {
-                    npc.coinValue += _param;
+                    npc.coinValue -= _param;
                 }
             }
         }
@@ -211,7 +211,7 @@ public class LxtDialogueManager : MonoBehaviour
             {
                 if (dog.name == _target)
                 {
-                    dog.healthValue += _param;
+                    dog.healthValue -= _param;
                 }
             }
         }
@@ -256,6 +256,4 @@ public class LxtDialogueManager : MonoBehaviour
             }
         }
     }
-   
-
 }
