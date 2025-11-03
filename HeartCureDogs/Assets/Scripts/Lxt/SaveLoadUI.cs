801692a68bb9a6064c30fddf7c85065c3d5ff822
Dkerain <165006578+Dkerain@users.noreply.github.com>
2025-11-03 18:23:00 +0800

    跳转场景实现
    改一下Scene界面和功能

--- HeartCureDogs/Assets/Scripts/Lxt/SaveLoadUI.cs
+++ HeartCureDogs/Assets/Scripts/Lxt/SaveLoadUI.cs
@@ -340,7 +340,7 @@ public class SaveLoadUI : MonoBehaviour
 
         // 加载游戏场景
         //SceneManager.LoadScene("GameScene");
-        UnityEngine.SceneManagement.SceneManager.LoadScene("GameScene");
+        UnityEngine.SceneManagement.SceneManager.LoadScene("HomeScene");
     }
     // 检查关键对象状态
     private void CheckCriticalObjects(string context)
